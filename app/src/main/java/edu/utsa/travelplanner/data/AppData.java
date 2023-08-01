84f53303186780debef886a142b139ba0bf92198
joelouie222 <joelouie222@gmail.com>
2023-07-31 23:23:16 -0500

    modified Database schedma for User_Model, included SecurityCode field that will be needed for password reset

--- app/src/main/java/edu/utsa/travelplanner/data/AppData.java
+++ app/src/main/java/edu/utsa/travelplanner/data/AppData.java
@@ -22,17 +22,22 @@ public class AppData extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE users (id INTEGER PRIMARY KEY, email TEXT, password TEXT)");
+        db.execSQL("CREATE TABLE users (id INTEGER PRIMARY KEY, email TEXT, password TEXT, securityCode TEXT)");
+        db.execSQL("CREATE TABLE userProfile (id INTEGER PRIMARY KEY, fName TEXT, lName TEXT, age TEXT, gender TEXT, address TEXT, phoneNumber TEXT)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop the old table if it exists.
         db.execSQL("DROP TABLE IF EXISTS users");
+        db.execSQL("DROP TABLE IF EXISTS userProfile");
 
         // Create the new table.
         onCreate(db);
     }
+
+
 
     public static AppData getInstance(Context context) {
         if (instance == null) {
@@ -41,12 +46,23 @@ public class AppData extends SQLiteOpenHelper {
         return instance;
     }
 
-    public void addUser(String email, String password){
+    // ------------------------------------  USER MODEL ---------------------------------------------------------------- //
+    public void addUser(String email, String password, String sc){
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("email", email);
         values.put("password", password);
+        values.put("securityCode", sc);
         db.insert("users", null, values);
+
+        values = new ContentValues();
+        values.put("fName", "");
+        values.put("lName", "");
+        values.put("age", "");
+        values.put("gender", "");
+        values.put("address", "");
+        values.put("phoneNumber", "");
+        db.insert("userProfile", null, values);
     }
 
     public List<User_Model> getUsers() {
@@ -57,22 +73,67 @@ public class AppData extends SQLiteOpenHelper {
             int id = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
             String email = cursor.getString(cursor.getColumnIndexOrThrow("email"));
             String password = cursor.getString(cursor.getColumnIndexOrThrow("password"));
-            users.add(new User_Model(id, email, password));
+            String securityCode = cursor.getString(cursor.getColumnIndexOrThrow("securityCode"));
+            users.add(new User_Model(id, email, password, securityCode));
         }
         return users;
     }
 
-    public void updateUser(int id, String email, String password) {
+    public void updateUser(String email, String password, String securityCode) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put("email", email);
-        values.put("password", password);
-        db.update("users", values, "id = ?", new String[]{String.valueOf(id)});
+
+        Cursor cursor = db.query("users", null, null, null, null, null, null);
+        while(cursor.moveToNext()) {
+            int id_DB = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
+            String email_DB = cursor.getString(cursor.getColumnIndexOrThrow("email"));
+            String password_DB = cursor.getString(cursor.getColumnIndexOrThrow("password"));
+            String securityCode_DB = cursor.getString(cursor.getColumnIndexOrThrow("securityCode"));
+            if (email_DB.equals(email)){
+                values.put("password", password);
+                values.put("securityCode", securityCode);
+                db.update("users", values, "id = ?", new String[]{String.valueOf(id_DB)});
+                break;
+            }
+        }
     }
 
     public void deleteUser(int id){
         SQLiteDatabase db = getWritableDatabase();
         db.delete("users", "id = ?", new String[]{String.valueOf(id)});
+        db.delete("userProfile", "id = ?", new String[]{String.valueOf(id)});
     }
+
+    // ------------------------------------  USER PROFILE MODEL ---------------------------------------------------------------- //
+    public UserProfile getUserProfile(int id){
+        SQLiteDatabase db = getWritableDatabase();
+        UserProfile profile_of_id = new UserProfile();
+
+        Cursor cursor = db.query("userProfile", null, null, null, null, null, null);
+        while(cursor.moveToNext()) {
+            int id_DB = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
+            String fName_DB = cursor.getString(cursor.getColumnIndexOrThrow("fName"));
+            String lName_DB = cursor.getString(cursor.getColumnIndexOrThrow("lName"));
+            String age_DB = cursor.getString(cursor.getColumnIndexOrThrow("age"));
+            String gender_DB = cursor.getString(cursor.getColumnIndexOrThrow("gender"));
+            String address_DB = cursor.getString(cursor.getColumnIndexOrThrow("address"));
+            String phoneNumber_DB = cursor.getString(cursor.getColumnIndexOrThrow("phoneNumber"));
+
+            if (id_DB == id){
+                profile_of_id.setfName(fName_DB);
+                profile_of_id.setlName(lName_DB);
+                profile_of_id.setAge(age_DB);
+                profile_of_id.setGender(gender_DB);
+                profile_of_id.setAddress(address_DB);
+                profile_of_id.setPhoneNumber(phoneNumber_DB);
+                return profile_of_id;
+            }
+        }
+        return profile_of_id;
+    }
+
+
+
+
 }
 
--- app/src/main/java/edu/utsa/travelplanner/data/User_Model.java
+++ app/src/main/java/edu/utsa/travelplanner/data/User_Model.java
@@ -5,42 +5,56 @@ public class User_Model {
         private String email;
         private String password;
 
-        public User_Model(int id, String email, String password) {
+        // NOT IN THE UML Diagram
+        private String securityCode;
+
+
+        public User_Model(int id, String email, String password, String securityCode) {
             this.id = id;
             this.email = email;
             this.password = password;
+            this.securityCode = securityCode;
         }
 
-    public int getId() {
-        return id;
-    }
+        public int getId() {
+            return id;
+        }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+        public void setId(int id) {
+            this.id = id;
+        }
 
-    public String getEmail() {
-        return email;
-    }
+        public String getEmail() {
+            return email;
+        }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+        public void setEmail(String email) {
+            this.email = email;
+        }
 
-    public String getPassword() {
-        return password;
-    }
+        public String getPassword() {
+            return password;
+        }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+        public void setPassword(String password) {
+            this.password = password;
+        }
 
-    @Override
-    public String toString() {
-        return "User_Model{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+        public String getSecurityCode() {
+            return securityCode;
+        }
+
+        public void setSecurityCode(String securityCode) {
+            this.securityCode = securityCode;
+        }
+
+        @Override
+        public String toString() {
+            return "User_Model{" +
+                    "id=" + id +
+                    ", email='" + email + '\'' +
+                    ", password='" + password + '\'' +
+                    ", securityCode='" + securityCode + '\'' +
+                    '}';
+        }
 }
