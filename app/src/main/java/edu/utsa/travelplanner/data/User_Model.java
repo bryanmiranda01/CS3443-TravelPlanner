a18f81679b4450ade50c88c58e3de1376c57ef2d
joelouie222 <joelouie222@gmail.com>
2023-08-01 15:49:05 -0500

    modifies User_Model to implement Parcelable, which will us to pass object from one activity to another

--- app/src/main/java/edu/utsa/travelplanner/data/User_Model.java
+++ app/src/main/java/edu/utsa/travelplanner/data/User_Model.java
@@ -1,6 +1,9 @@
 package edu.utsa.travelplanner.data;
 
-public class User_Model {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User_Model implements Parcelable {
         private int id;
         private String email;
         private String password;
@@ -16,45 +19,70 @@ public class User_Model {
             this.securityCode = securityCode;
         }
 
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getSecurityCode() {
-            return securityCode;
-        }
-
-        public void setSecurityCode(String securityCode) {
-            this.securityCode = securityCode;
+        @Override
+        public int describeContents() {
+            return 0;
         }
 
         @Override
-        public String toString() {
-            return "User_Model{" +
-                    "id=" + id +
-                    ", email='" + email + '\'' +
-                    ", password='" + password + '\'' +
-                    ", securityCode='" + securityCode + '\'' +
-                    '}';
+        public void writeToParcel(Parcel parcel, int flags) {
+            parcel.writeInt(id);
+            parcel.writeString(email);
+            parcel.writeString(password);
+            parcel.writeString(securityCode);
         }
+
+        public static final Parcelable.Creator<User_Model> CREATOR = new Parcelable.Creator<User_Model>() {
+            @Override
+            public User_Model createFromParcel(Parcel parcel) {
+                return new User_Model(parcel.readInt(), parcel.readString(), parcel.readString(), parcel.readString());
+            }
+
+            @Override
+            public User_Model[] newArray(int size) {
+                return new User_Model[size];
+            }
+        };
+
+        public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getEmail() {
+                return email;
+            }
+
+            public void setEmail(String email) {
+                this.email = email;
+            }
+
+            public String getPassword() {
+                return password;
+            }
+
+            public void setPassword(String password) {
+                this.password = password;
+            }
+
+            public String getSecurityCode() {
+                return securityCode;
+            }
+
+            public void setSecurityCode(String securityCode) {
+                this.securityCode = securityCode;
+            }
+
+            @Override
+            public String toString() {
+                return "User_Model{" +
+                        "id=" + id +
+                        ", email='" + email + '\'' +
+                        ", password='" + password + '\'' +
+                        ", securityCode='" + securityCode + '\'' +
+                        '}';
+            }
 }
